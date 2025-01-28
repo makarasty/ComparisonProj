7de24891fb0bb60631ea27abd370add1120c5bf0
makarasty <makarasty123@gmail.com>
2025-01-28 19:25:03 +0200

    Update productsAggregator.ts

--- server/src/services/productsAggregator.ts
+++ server/src/services/productsAggregator.ts
@@ -1,16 +1,16 @@
 import { IProduct } from "../interfaces/Product";
+
 //import { getFakeStoreApiProducts } from "./api/trash/fakestoreApiService";
-import { getAllDummyJsonProducts } from "./api/dummyJsonService";
-import { getAllFakeStoreProducts } from "./api/fakestoreApiService";
 //import { getEscuelaJsProducts } from "./api/trash/escuelajsService";
 //import { getBeeceptorProducts } from "./api/trash/beeceptorService";
+import { getAllDummyJsonProducts } from "./api/dummyJsonService";
+import { getAllFakeStoreProducts } from "./api/fakestoreApiService";
 
 export async function getAllProducts(): Promise<IProduct[]> {
-	const [dummyJsonProducts, fakeStoreProducts] =
-		await Promise.all([
-			getAllDummyJsonProducts(),
-			getAllFakeStoreProducts()
-		]);
+	const [dummyJsonProducts, fakeStoreProducts] = await Promise.all([
+		getAllDummyJsonProducts(),
+		getAllFakeStoreProducts(),
+	]);
 
 	return [...dummyJsonProducts, ...fakeStoreProducts];
 }
