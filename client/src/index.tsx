6827856f71f3deee9cc0ec3388072720bfc37884
makarasty <makarasty123@gmail.com>
2025-02-07 17:56:01 +0200

    better compiler

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // ваші глобальні стилі, якщо потрібні
+import "./index.css";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,30 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 80,
+	plugins: [
+		react(),
+	],
+	build: {
+		minify: "terser",
+		sourcemap: false,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor: ["react", "react-dom"],
+				},
+			},
+		},
+		target: "esnext",
+		cssCodeSplit: true,
+		reportCompressedSize: false,
+	},
+	optimizeDeps: {
+		include: ["react", "react-dom"],
+		esbuildOptions: {
+			target: "esnext",
+		},
+	},
+	esbuild: {
+		logOverride: { "this-is-undefined-in-esm": "silent" },
 	},
 });
